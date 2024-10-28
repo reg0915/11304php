76f6197a8151e060f8dae3cf183003ed4792357c
reg <reg8740223@gmail.com>
2024-10-28 10:56:38 +0800

    Remove nested repository php.-

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>計算BMI</title>
+</head>
+<body>
+    <h1>計算BMI</h1>
+<form action="result.php" method>
+<div>
+<label for="height">身高:</label>
+<input type="number" name="height" id="height">
+</div>
+
+<div>
+    <label for="weight">體重:</label>
+    <input type="number" name="weight" id="weight">
+</div>
+</form>
+
+
+
+
+
+
+</body>
+</html>
